80a2b89c688ca9a14ed55f49a078ddcd8843b6c9
Mr-Sena <jsenna985@gmail.com>
2021-12-02 23:32:11 -0300

    DML Básico

---
+++ mySQL-Workbench/Scripts/tbCreate.sql
@@ -0,0 +1,14 @@
+Create table tb_Cliente 
+	(CPF varchar(11), 
+	Nome varchar(100),
+    Endereco1 varchar (150),
+    Endereco2 varchar (150),
+    Bairo varchar (50),
+    Cidade varchar(50), 
+    Estado varchar(50),
+    CEP varchar(8), 
+    Idade smallint, 
+    Sexo varchar(1), 
+    `Limite Credito` float,
+    Volume_Compra float,
+    is_primeira_compra bit(1))
